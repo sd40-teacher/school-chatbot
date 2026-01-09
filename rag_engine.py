755da4a7d5908d48cdcd26d2533100fa67a79026
sd40-teacher <sd40@sdglobal.sen.hs.kr>
2026-01-09 22:22:30 +0900

    Update rag_engine.py
    Python
    
    from langchain.text_splitter import RecursiveCharacterTextSplitter
    수정 코드:
    
    Python
    
    # 최신 버전에서는 text_splitter가 별도 패키지로 분리되었습니다.
    from langchain_text_splitters import RecursiveCharacterTextSplitter

--- rag_engine.py
+++ rag_engine.py
@@ -1,5 +1,5 @@
 from langchain_community.document_loaders import PyPDFDirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.chains import RetrievalQA
